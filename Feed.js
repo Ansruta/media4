c828f2d3e21339647b2928202ad558f65a73d393
Ansruta <75419631+Ansruta@users.noreply.github.com>
2021-12-20 17:09:57 +0530

    Add files via upload

---
+++ Feed.js
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import {Text,View,StyleSheet, StatusBar, FlatList} from 'react-native';
+import PostCard from './PostCard';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+
+export default class Feed extends React.Component{
+    renderItem=({item:post})=>{
+        return <PostCard post={post} navigation={this.props.navigation}></PostCard>
+    };
+    keyExtractor=(item,index)=>index.toString();
+    render(){
+        return(
+            <View style={styles.contain}>
+                <SafeAreaView style={styles.droidSafeArea}/>
+                    <View style={styles.appTitle}>
+                        <View style={styles.appIcon}>
+                            <Image source={require("./logo.png")}
+                                    style={styles.iconImage}/>
+                        </View>
+                        <View style={styles.appTitleTextContainer}>
+                            <Text style={styles.appTitleText}> Spectagram </Text>
+                        </View>
+                    </View>
+                    <View styles={styles.cardContainer}>
+                        <FlatList
+                        keyExtractor={this.keyExtractor}
+                        data={posts}
+                        renderItem={this.renderItem}
+                        />
+                    </View>
+            </View>
+        )
+    }
+}
+const styles=StyleSheet.create({
+contain:{
+    flex:1,
+    backgroundColor:"black"
+},
+droidSafeArea:{
+    marginTop:Platfoem.OS==="android"?StatusBar.currentHeight:RFValue(30)
+},
+appTitle:{
+    flex:0.07,
+    flexDirection:"row"
+},
+appIcon:{
+    flex:0.2,
+    justifyContent:"center",
+    alignItems:"center"
+},
+iconImage:{
+    width:"100%",
+    height:"100%",
+    resizeMode:"contain"
+},
+appTitleTextContainer:{
+    flex:0.8,
+    justifyContent:"center"
+},
+appTitleText:{
+    color:"white",
+    fontSize:RFValue(28)
+},
+cardContainer:{
+    flex:0.05
+}
+})
---
+++ StackNavigator.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import TabNavigator from './TabNavigator';
+import Post from './PostCard';
+import {createStackNavigation} from '@react-navigation/stack';
+
+const Stack=createStackNavigation();
+const StackNavigator=()=>{
+return(
+    <Stack.TabNavigator initialRouteName="Home" screenOptions={{headerShown=false}}>
+        <Stack.Screen name={"Home"} component={TabNavigator}/>
+        <Stack.Screen name={"Post"} component={Post}/>
+    </Stack.TabNavigator>
+)
+}
+export default StackNavigator;
